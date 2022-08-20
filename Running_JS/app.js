81d7d2ad71eeb834c19796fc06d334945bb9a2c2
Minwoo Gwak <mgwak2@illinois.edu>
2022-08-20 11:43:04 +0000

    if statement

--- Running_JS/app.js
+++ Running_JS/app.js
@@ -1,1 +1,6 @@
-console.log("Before C")
+console.log("BEFORE CONDITIONAL")
+let random = Math.floor(Math.random()*5);
+if(random >= 2) {
+  console.log("it is greater or equal than 2")
+}
+console.log("it is not greater than equal than 2")
