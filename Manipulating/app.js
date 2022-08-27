7f6c24b3ec93a1fa64ff5c5902aa8d3cb8728267
Minwoo Gwak <mgwak2@illinois.edu>
2022-08-27 12:06:10 +0000

    get element by id, tagname, classname

--- Manipulating/app.js
+++ Manipulating/app.js
@@ -1,21 +1,36 @@
-const allLinks = document.querySelectorAll('a');
-
-const allImages = document.getElementsByTagName('img')
+const allLinks = document.querySelectorAll("a");
 
 // for (let link of allLinks) {
 //     link.innerText = 'I AM A LINK!!!!'
 // }
 
+/**
+ * get element by id
+ */
 
-console.log(`=======ALL ELEMENT OF IMAGE SOURCE==========`)
-for(let img of allImages) {
-    console.log(img.src)
+const toc = document.getElementById("toc");
+console.log(`=======ALL ELEMENT OF IMAGE SOURCE USING TAG NAME==========`);
+console.dir(toc)
+/**
+ * get element by tag name
+ */
+const allImages = document.getElementsByTagName("img");
+console.log(`=======ALL ELEMENT OF IMAGE SOURCE USING TAG NAME==========`);
+for (let img of allImages) {
+  console.log(img.src);
 }
 
-
+/**
+ * get element by class name
+ */
+const allImages2 = document.getElementsByClassName("square");
+console.log("========ALL ELEMENT OF IMAGE USING CLASSNAME");
+for (let img of allImages2) {
+  console.log(img.src);
+}
 
 for (let link of allLinks) {
-    link.style.color = 'rgb(0, 108, 134)';
-    link.style.textDecorationColor = 'magenta';
-    link.style.textDecorationStyle = 'wavy'
+  link.style.color = "rgb(0, 108, 134)";
+  link.style.textDecorationColor = "magenta";
+  link.style.textDecorationStyle = "wavy";
 }
