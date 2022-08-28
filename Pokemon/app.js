eae4e8d609d227987ddc91f01b4729c24c8132f0
Minwoo Gwak <mgwak2@illinois.edu>
2022-08-28 14:17:54 +0000

    pokemon site completed

--- Pokemon/app.js
+++ Pokemon/app.js
@@ -1,19 +1,20 @@
 // https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
 
-const container = document.querySelector('#container');
-const baseURL = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/'
+const baseURL =
+  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
 
+const container = document.querySelector("#container");
 
-for (let i = 1; i <= 151; i++) {
-    const pokemon = document.createElement('div');
-    pokemon.classList.add('pokemon');
-    const label = document.createElement('span');
-    label.innerText = `#${i}`;
-    const newImg = document.createElement('img');
-    newImg.src = `${baseURL}${i}.png`
+for (let i = 1; i <= 100; i++) {
+  const pokemon = document.createElement('div');
+  pokemon.classList.add('pokemon');
 
-
-    pokemon.appendChild(newImg);
-    pokemon.appendChild(label);
-    container.appendChild(pokemon)
+  const label = document.createElement('span');
+  label.innerText = `#${i}`
+  const newImg = document.createElement("img");
+  newImg.src =
+    `${baseURL}${i}.png`;
+  pokemon.appendChild(newImg)
+  pokemon.appendChild(label)
+  container.appendChild(pokemon);
 }
