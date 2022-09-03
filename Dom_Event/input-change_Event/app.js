d561312a28f405dd6ad151d6f4f52aa0031f685e
Minwoo Gwak <mgwak2@illinois.edu>
2022-09-03 08:54:35 +0000

    input event and change event

---
+++ Dom_Event/input-change_Event/app.js
@@ -0,0 +1,8 @@
+document.querySelector('input').addEventListener('change', () => {
+  console.log('changed!!')
+})
+
+
+document.querySelector('input').addEventListener('input', () => {
+  console.log('input!!')
+})
