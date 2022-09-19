0d325c91c14a8927751632b3fe275df634ef3993
Minwoo Gwak <mgwak2@illinois.edu>
2022-09-19 10:07:50 +0000

    some get method to set extra

--- Express/FirstApp/index.js
+++ Express/FirstApp/index.js
@@ -2,10 +2,23 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-app.use((req, res) => {
+/* app.use((req, res) => {
   console.log("NEW REQUEST");
-  res.send("<h1>This is response from a server</h1>")
+  res.send("<h1>This is response from a server</h1>");
+}); */
+
+app.get("/", (req, res) => {
+  res.send("this is the hompage");
 });
+
+app.get("/cats", (req, res) => {
+  res.send("MEOW!!");
+});
+
+app.get("/dogs", (req, res) => {
+  res.send("WOOOF!!");
+});
+
 
 app.listen(port, () => {
   console.log("listening on port 3000");
