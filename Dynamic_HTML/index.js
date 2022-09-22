8a8a27370c08e6ff501e845fa0819f1b4725df88
Minwoo Gwak <mgwak2@illinois.edu>
2022-09-22 12:16:37 +0000

    Passing data to template

--- Dynamic_HTML/index.js
+++ Dynamic_HTML/index.js
@@ -3,10 +3,15 @@ const path = require("path");
 const app = express();
 
 app.set("view engine", "ejs");
-app.set('views', path.join(__dirname, '/views'))
+app.set("views", path.join(__dirname, "/views"));
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
+});
+
+app.get("/rand", (req, res) => {
+  const randNum = Math.floor(Math.random() * 10) + 1;
+  res.render("random.ejs", { rand: randNum });
 });
 
 app.listen(3000, () => {
