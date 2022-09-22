bc70666fc81cd313fb8f027e5cca23c6ca70cf6f
Minwoo Gwak <mgwak2@illinois.edu>
2022-09-22 12:06:05 +0000

    added views directory

--- Dynamic_HTML/index.js
+++ Dynamic_HTML/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
-
+const path = require("path");
 const app = express();
 
 app.set("view engine", "ejs");
+app.set('views', path.join(__dirname, '/views'))
 
 app.get("/", (req, res) => {
   res.render("home.ejs");
