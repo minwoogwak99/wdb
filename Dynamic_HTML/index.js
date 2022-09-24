2b74c8f8f40543f2b274ef8d9f6eaa01ef25c0db
Minwoo Gwak <mgwak2@illinois.edu>
2022-09-24 10:36:29 +0000

    static asset in express

--- Dynamic_HTML/index.js
+++ Dynamic_HTML/index.js
@@ -4,6 +4,9 @@ const app = express();
 const redditData = require("./data.json");
 console.log(redditData);
 
+app.use(express.static(path.join(__dirname, "public")))
+
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
