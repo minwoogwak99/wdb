586dad79e1149c476df567421bf705d724c5b3f7
Minwoo Gwak <mgwak2@illinois.edu>
2022-09-25 10:39:35 +0000

    EJS and Partial

--- Dynamic_HTML/index.js
+++ Dynamic_HTML/index.js
@@ -2,36 +2,38 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const redditData = require("./data.json");
-console.log(redditData);
 
-app.use(express.static(path.join(__dirname, "public")))
-
+app.use(express.static(path.join(__dirname, "public")));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
+//home
 app.get("/", (req, res) => {
-  res.render("home.ejs");
+  res.render("home.ejs", {title: 'HOME'});
 });
 
+//cats
 app.get("/cats", (req, res) => {
   const cats = ["Blue", "Rocket", "Monty", "Stephanie", "Winston"];
-  res.render("cats", { cats });
+  res.render("cats", { cats, title: cats });
 });
 
+//subreddit
 app.get("/r/:subreddit", (req, res) => {
   const { subreddit } = req.params;
   const data = redditData[subreddit];
-  if(data) {
-    res.render("subreddit", { ...data });
-  } else{
-    res.render('notFound', {subreddit})
+  if (data) {
+    res.render("subreddit", { ...data, title: subreddit });
+  } else {
+    res.render("notFound", { subreddit, title: subreddit });
   }
 });
 
+//Random 
 app.get("/rand", (req, res) => {
   const randNum = Math.floor(Math.random() * 10) + 1;
-  res.render("random.ejs", { rand: randNum });
+  res.render("random.ejs", { randNum, title: 'random'});
 });
 
 app.listen(3000, () => {
