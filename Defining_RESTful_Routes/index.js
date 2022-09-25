578881aa19a1a01c475f2e4594ad90d8f4c762e5
Minwoo Gwak <mgwak2@illinois.edu>
2022-09-25 14:56:20 +0000

    parsing the request body part 1

--- Defining_RESTful_Routes/index.js
+++ Defining_RESTful_Routes/index.js
@@ -1,16 +1,23 @@
 const express = require("express");
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('HOME')
-})
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/tacos', (req, res) => {
-  res.send('GET /tacos response')
-})
-app.post('/tacos', (req, res) => {
-  res.send('POST /tacos response')
-})
+app.get("/", (req, res) => {
+  res.send("HOME");
+});
+
+app.get("/tacos", (req, res) => {
+  res.send("GET /tacos response");
+});
+
+app.post("/tacos", (req, res) => {
+  console.log(req.body);
+  const meat = req.body.meat;
+  const qtty = req.body.qty;
+  console.log(`Here is your ${qtty} ${meat} tacos!`);
+  res.send(`Here is your ${qtty} ${meat} tacos!`);
+});
 
 app.listen(3000, () => {
   console.log("listen on port 3000");
