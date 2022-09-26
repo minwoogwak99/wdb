d615373decfa1d339b79fa0f68814b21007267ed
Minwoo Gwak <mgwak2@illinois.edu>
2022-09-26 12:14:21 +0000

    express redirect

--- Defining_RESTful_Routes/index.js
+++ Defining_RESTful_Routes/index.js
@@ -37,7 +37,7 @@ app.get("/comments/new", (req, res) => {
 app.post("/comments", (req, res) => {
   const { username, comment } = req.body;
   comments.push({ username, comment });
-  res.send("It worked!!");
+  res.redirect('/comments')
 });
 
 app.get("/comments", (req, res) => {
