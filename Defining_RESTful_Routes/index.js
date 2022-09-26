ea062fed4e0acd4c30d1be2d150de38b933cb492
Minwoo Gwak <mgwak2@illinois.edu>
2022-09-26 11:43:39 +0000

    RESTful comments index

--- Defining_RESTful_Routes/index.js
+++ Defining_RESTful_Routes/index.js
@@ -1,10 +1,39 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-  res.send("HOME");
+const comments = [
+  {
+    username: "Todd",
+    comments: "this is so funny",
+  },
+  {
+    username: "Alex",
+    comments: "WTF????",
+  },
+  {
+    username: "James",
+    comments: "LOLOLOLOLOL",
+  },
+  {
+    username: "Sarha",
+    comments: "GOAT!!",
+  },
+  {
+    username: "Tom",
+    comments: "what...???",
+  },
+];
+
+
+
+app.get("/comments", (req, res) => {
+  res.render("comments/index", {comments});
 });
 
 app.get("/tacos", (req, res) => {
@@ -13,12 +42,20 @@ app.get("/tacos", (req, res) => {
 
 app.post("/tacos", (req, res) => {
   console.log(req.body);
-  const meat = req.body.meat;
-  const qtty = req.body.qty;
-  console.log(`Here is your ${qtty} ${meat} tacos!`);
-  res.send(`Here is your ${qtty} ${meat} tacos!`);
+  const { meat, qty } = req.body;
+  console.log(meat);
+  console.log(qty);
+  res.send(`Here is your ${qty} ${meat} tacos!`);
 });
 
 app.listen(3000, () => {
   console.log("listen on port 3000");
 });
+
+/* 
+GET /comments - list all comments
+POST /comments - Create a new comment
+GET /comments/:id - GET one comment using ID
+PATCH /comments/:id - Update one comment 
+DELETE /comments/:id - Destroy one comment
+*/
