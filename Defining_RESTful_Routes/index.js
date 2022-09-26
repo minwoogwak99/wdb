96da2352e7f8d68cdb40d8a63e51c27df38b8f25
Minwoo Gwak <mgwak2@illinois.edu>
2022-09-26 14:06:50 +0000

    update comment with patch request

--- Defining_RESTful_Routes/index.js
+++ Defining_RESTful_Routes/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const { v4: uuid } = require("uuid");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -8,36 +9,27 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 const comments = [
-  {
-    username: "Todd",
-    comment: "this is so funny",
-  },
-  {
-    username: "Alex",
-    comment: "WTF????",
-  },
-  {
-    username: "James",
-    comment: "LOLOLOLOLOL",
-  },
-  {
-    username: "Sarha",
-    comment: "GOAT!!",
-  },
-  {
-    username: "Tom",
-    comment: "what...???",
-  },
+  { id: uuid(), username: "Todd", comment: "this is so funny" },
+  { id: uuid(), username: "Alex", comment: "WTF????" },
+  { id: uuid(), username: "James", comment: "LOLOLOLOLOL" },
+  { id: uuid(), username: "Sarha", comment: "GOAT!!" },
+  { id: uuid(), username: "Tom", comment: "what...???" },
 ];
 
 app.get("/comments/new", (req, res) => {
   res.render("comments/new");
 });
 
+app.get("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find((c) => c.id === id);
+  res.render("comments/show", { comment });
+});
+
 app.post("/comments", (req, res) => {
   const { username, comment } = req.body;
-  comments.push({ username, comment });
-  res.redirect('/comments')
+  comments.push({ username, comment, id: uuid() });
+  res.redirect("/comments");
 });
 
 app.get("/comments", (req, res) => {
@@ -45,6 +37,18 @@ app.get("/comments", (req, res) => {
   res.render("comments/index", { comments });
 });
 
+app.patch("/comments/:id", (req, res) => {
+  const { id } = req.params;
+  const newCommentText = req.body.comment;
+  console.log('new:',newCommentText);
+  const foundcomment = comments.find((c) => c.id === id);
+  console.log(foundcomment);
+  foundcomment.comment = newCommentText;
+
+  res.redirect("/comments");
+});
+
+//TACOS======================================================================
 app.get("/tacos", (req, res) => {
   res.send("GET /tacos response");
 });
